b1958c7792197188164e7cd4a888e35cbbf17282
umaiyer <uma.paranjape@gmail.com>
2019-08-19 19:55:22 -0700

    Added enhance and black&white buttons. Not all dates seem to have valid metadata. Tested with a couple good dates in 2017 (eg April 11,2017)

--- src/main/java/hellofx/BlueMarbleController.java
+++ src/main/java/hellofx/BlueMarbleController.java
@@ -1,41 +1,104 @@
 package hellofx;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.InputStream;
-import java.net.URL;
-import java.nio.file.Paths;
+import java.time.LocalDate;
 
 import javax.imageio.ImageIO;
 
-import org.apache.commons.io.IOUtils;
 import org.curiousworks.BlueMarble;
-import org.json.JSONObject;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class BlueMarbleController {
+	boolean isEnhanced=false;
+	BufferedImage imgBuf=null;	
 
 	@FXML
 	private ImageView image;
 
 	@FXML
 	private DatePicker datePicker;
+	
+	@FXML
+	private ToggleButton toggleEnhance;
+	
+	@FXML
+	private Button displayBlackAndWhite;
+	
+	@FXML
+	private Alert notAvailableAlert;
 
 	@FXML
 	void updateDate(ActionEvent event) {
-		
-		BlueMarble blueMarble = new BlueMarble();
-//		blueMarble.setDate(datePicker.getValue().getYear() + "-0" + datePicker.getValue().getMonthValue() + "-" + datePicker.getValue().getDayOfMonth());
-		blueMarble.setDate("2018-0" + datePicker.getValue().getMonthValue() + "-" + datePicker.getValue().getDayOfMonth());
-		blueMarble.setEnhanced(true);
-//		Image value = new Image(BlueMarble.getMostRecentImage());
-		image.setImage(new Image(blueMarble.getImage()));
+		isEnhanced = false;
+		updateImage(false);
 	}
 
+	@FXML
+	void displayEnhanced(ActionEvent event) {
+		if (!isEnhanced) {
+			updateImage(true);
+			isEnhanced=true;
+		}			
+		else {
+			updateImage(false);
+			isEnhanced=false;
+		}
+		
+	}
+	
+	@FXML
+	void convertBW(ActionEvent event) {
+		if (imgBuf == null) return;
+		BufferedImage convBuf = new BufferedImage(imgBuf.getWidth(), imgBuf.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
+		Graphics2D graphic = convBuf.createGraphics();
+		graphic.drawImage(imgBuf, 0, 0, Color.WHITE,null);
+		graphic.dispose();
+		image.setImage(SwingFXUtils.toFXImage(convBuf, null));
+	}
+	
+	private void updateImage(boolean enhance) {
+		BlueMarble blueMarble = new BlueMarble();
+		blueMarble.setDate(datePicker.getValue().getYear() + "-0" + datePicker.getValue().getMonthValue() + "-" + datePicker.getValue().getDayOfMonth());
+		if (datePicker.getValue().isAfter(LocalDate.now())) {
+			notAvailableAlert = new Alert(AlertType.ERROR);
+			notAvailableAlert.setContentText("Please select dates in the past. Cannot retrieve images for dates in the future");
+			notAvailableAlert.showAndWait();
+			return;			
+		}		
+		else if (datePicker.getValue().getYear() > 2018 || (datePicker.getValue().getMonthValue() > 6 && datePicker.getValue().getYear() == 2018 ))
+			toggleEnhance.setDisable(true);
+		else
+			toggleEnhance.setDisable(false);
+		
+		if (enhance) blueMarble.setEnhanced(enhance);
+		
+//		Image value = new Image(BlueMarble.getMostRecentImage());
+		try { 
+			InputStream imgInputStream = blueMarble.getImage();
+			// Update imgBuf to most recent file
+			imgBuf = ImageIO.read(imgInputStream);
+			image.setImage(new Image(blueMarble.getImage()));
+		} catch (Exception e) {
+			e.printStackTrace();
+			notAvailableAlert = new Alert(AlertType.ERROR);
+			notAvailableAlert.setContentText("Error retrieving image");
+			notAvailableAlert.showAndWait();
+			return;
+		}
+		
+	}
 
 }
